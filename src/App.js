26a5309ce0f177bea54a229ff46bcb7be92b2587
vDurin <king.moifur@gmail.com>
2023-05-15 10:49:22 +0200

    lista utenti

--- src/App.js
+++ src/App.js
@@ -1,107 +1,69 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Button } from 'react-bootstrap';
 import Menu from './Menu';
 import Footer from './Footer';
 
-// Gabriele
-// Giuseppe
 
-const LoginForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState(null);
-  const [state, setState] = useState(false);
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Crea l'oggetto dati da inviare all'API
-    const data = {
-      email: email,
-      password: password
-    };
-
-    // Effettua la richiesta all'API
-    fetch('https://645e112a12e0a87ac0e6b4cf.mockapi.io/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(responseData => {
-      // Gestisci la risposta dall'API 
-      setState(true);
-      //setResponsedata(responseData.name)
-      setName('Giuseppe');
-      console.log('Risposta dall\'API:', responseData);
-    })
-    .catch(error => {
-      // Gestisci l'errore
-      setName('nessuno')
-      console.error('Errore nella richiesta:', error);
-      setState(false)
-    });
-  };
   useEffect(() => {
-  }, [state]);
-  if (!state) {
-    return (
-     
-      <div class="container">
-         {state}
-        <Menu />
-      <div className="card">
-        <div className="card-body">
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formEmail">
-              <Form.Label>Email:</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Inserisci l'email"
-                value={email}
-                onChange={handleEmailChange}
-              />
-            </Form.Group>
+    fetch('https://645e112a12e0a87ac0e6b4cf.mockapi.io/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.log(error));
+  }, []);
 
-            <Form.Group controlId="formPassword">
-              <Form.Label>Password:</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Inserisci la password"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </Form.Group>
+  const handleDelete = (userId) => {
+    fetch(`https://645e112a12e0a87ac0e6b4cf.mockapi.io/users/id=${userId}`, {
+      method: 'DELETE'
+    })
+      .then(response => {
+        if (response.ok) {
+          // Rimuovi l'utente dall'array degli utenti nello stato
+          setUsers(prevUsers => prevUsers.filter(user => user.id !== userId));
 
-            <Button variant="primary" type="submit">
-              Accedi
-            </Button>
-          </Form>
-        </div>
-      </div>
-      <Footer />
-      </div>
 
-    );
-  } else {
-    return  <div class="container">
+          fetch('https://645e112a12e0a87ac0e6b4cf.mockapi.io/users')
+          .then(response => response.json())
+          .then(data => setUsers(data))
+          .catch(error => console.log(error));
+
+        } else {
+           setError('Errore durante l\'eliminazione del record');
+        }
+      })
+      .catch(error => console.log(error));
+  };
+  const handleCloseError = () => {
+    setError(null);
+  };
+
+  return (
+    <div>
     <Menu />
-    <div>Benvenuto {name}
-    </div>
+    <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}> {user.id} - {user.email} - {user.name} 
+           <button onClick={() => handleDelete(user.id)}>Elimina</button>
+          </li>
+        ))}
+      </ul>
+
+      {error && (
+        <div className="popup">
+          <p>{error}</p>
+          <button onClick={handleCloseError}>Chiudi</button>
+        </div>
+      )}
+
     <Footer />
     </div>
-  }
+  );
 };
 
-export default LoginForm;
+export default UserList;
