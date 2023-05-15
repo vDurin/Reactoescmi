6179c206444cac28328f0b2c668f43bbe5dacd64
Gabriele520 <133634914+Gabriele520@users.noreply.github.com>
2023-05-15 09:29:43 +0200

    ciao

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Form, Button } from 'react-bootstrap';
 import Menu from './Menu';
 import Footer from './Footer';
 
+// Gabriele
+
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
