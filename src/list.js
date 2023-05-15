6eb95357ed855484d7b442a11c0e0f74b768dfac
vDurin <king.moifur@gmail.com>
2023-05-15 12:16:01 +0200

    patch lista 1.0

--- src/list.js
+++ src/list.js
@@ -3,18 +3,23 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 
 const Lista = ({ users, delete: handleDelete }) => {
+
+  const handleClickDelete = (userId) => {
+    handleDelete(userId);
+  };
+
   return (   
       <ListGroup>
-      <ListGroup.Item>
-      <h1>User List</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}> {user.id} - {user.email} - {user.name} 
-           <button onClick= {handleDelete(user.id)}>Elimina</button>
-          </li>
-        ))}    
-      </ul>
-      </ListGroup.Item>
+        <ListGroup.Item>
+        <h1>User List</h1>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}> {user.id} - {user.email} - {user.name} 
+            <button onClick={() => handleClickDelete(user.id)}>Elimina</button>
+            </li>
+          ))}    
+        </ul>
+        </ListGroup.Item>
       </ListGroup>
   ); 
 };
